2d5f7d085e2fdffc0708c085b9a80d5353b832a6
chiara011 <36208248+chiara011@users.noreply.github.com>
2020-08-04 18:49:29 +0200

    first library - sound - update

---
+++ rpi_parser/translator.py
@@ -0,0 +1,13 @@
+import scipy
+
+import library
+
+# library.playsound(440, "sine", 1, 1000)
+# library.playsound(200, "square", 100, 1000)
+# library.playsound(200, "sawtooth", 100, 1000)
+# library.playsound(200, "sine", 100, 1000)
+# library.playsound(200, "triangle", 100, 1000)
+# library.playsound(200, "sine", 30, 1000)
+
+
+print(scipy.signal.sawtooth((1 / 440)))
